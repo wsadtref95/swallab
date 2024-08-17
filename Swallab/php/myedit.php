c145d817f515fac8c2f9976da380681180c07f39
yahsuan1124 <julia175175@gmail.com>
2024-08-17 21:34:48 +0800

    食記

---
+++ Swallab/php/myedit.php
@@ -0,0 +1,28 @@
+<?php
+$host = 'localhost';
+$dbname = "swallab";
+$user = "root";
+$password = "";
+$dataMid = $_GET["id"];
+
+try {
+    // 連接資料庫
+    $db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+
+    // 從資料庫中獲取資料
+    // $sql = "SELECT * FROM message WHERE id =$dataMid;";
+    $sql = "SELECT * from notescomments where id =$dataMid;";
+    $stmt = $db->query($sql, PDO::FETCH_ASSOC);
+    $rows = $stmt->fetchAll();
+   
+    // 設定內容類型
+    header('Content-Type: text/html; charset=utf-8');
+    // print_r($rows);
+    foreach ($rows as $row) {
+        $messageId =($row['content']); 
+        print $messageId;
+    }
+} catch (PDOException $e) {
+    echo '連接失敗: ' . $e->getMessage();
+}
+?>
