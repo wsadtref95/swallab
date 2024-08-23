eb635f10d8bdeb85c76a15c95d2bb1dcc1e9482c
L1zzlim <167948071+L1zzlim@users.noreply.github.com>
2024-08-23 11:42:20 +0800

    data of favorites completed

--- MySwallab/app/Http/Resources/NotesFavoriteResource.php
+++ MySwallab/app/Http/Resources/NotesFavoriteResource.php
@@ -3,25 +3,29 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use DateTime;
 
 class NotesFavoriteResource extends JsonResource
 {
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
+            'id' => optional($this)->id,
             'type' => 'noteFavorite',
             'note' => [
-                'title' => $this->MemberNotes->title,
-                'main_photo' => $this->MemberNotes->main_photo,
+                'title' => optional(optional($this)->MemberNotes)->title,
+                'main_photo' => optional(optional($this)->MemberNotes)->main_photo,
+                'content' => optional(optional($this)->MemberNotes)->content,
+                'visit_date' => optional(optional($this)->MemberNotes)->visited_at_datetime ? (new DateTime(optional($this)->MemberNotes->visited_at_datetime))->format('Y-m-d') : null,
+                'count' => optional(optional($this)->MemberNotes)->count,
                 'restaurant' => [
-                    'name' => $this->MemberNotes->RestInfos->Users->name,
-                    'address' => $this->MemberNotes->RestInfos->address,
-                    'avg_price' => $this->MemberNotes->RestInfos->avg_price,
-                    'wd_operating' => $this->MemberNotes->RestInfos->wd_operating,
+                    'name' => optional(optional(optional($this)->MemberNotes)->RestInfos)->Users ? optional($this->MemberNotes->RestInfos->Users)->name : null,
+                    'address' => optional(optional($this->MemberNotes)->RestInfos)->address,
+                    'avg_price' => optional(optional($this->MemberNotes)->RestInfos)->avg_price,
+                    'wd_operating' => optional(optional($this->MemberNotes)->RestInfos)->wd_operating,
                 ],
             ],
-            'created_at' => $this->created_at->toDateTimeString(),
+            'created_at' => optional(optional($this)->created_at)->toDateTimeString(),
         ];
     }
 }
